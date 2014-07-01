7169430d2dbebaa7ae6d117a62525e03cab1a17a
Sjors Provoost <sjors@sprovoost.nl>
2014-07-01 14:07:12 +0200

    Created CocoaTouch Framework

---
+++ UInt256/UInt256.h
@@ -0,0 +1,19 @@
+//
+//  UInt256.h
+//  UInt256
+//
+//  Created by Sjors Provoost on 01-07-14.
+//  Copyright (c) 2014 Crypto Coin Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UInt256.
+FOUNDATION_EXPORT double UInt256VersionNumber;
+
+//! Project version string for UInt256.
+FOUNDATION_EXPORT const unsigned char UInt256VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UInt256/PublicHeader.h>
+
+
