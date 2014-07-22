120b6a6c176e7c661315b53aa85ff24484e71ce7
Sjors Provoost <sjors@sprovoost.nl>
2014-07-22 10:14:32 +0200

    Remove bridging header (Beta 4)

--- UInt256Mac/UInt256Mac.h
+++ UInt256Mac/UInt256Mac.h
@@ -16,4 +16,11 @@ FOUNDATION_EXPORT const unsigned char UInt256MacVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UInt256Mac/PublicHeader.h>
 
+// Takes two arrays which 8 UInt32's each. They are exposed more for testing purposes than speed.
+void subtract(uint32_t result[], uint32_t lhs[], uint32_t rhs[]);
 
+// Takes two arrays which 8 UInt32's each and returns the result as an array.
+uint32_t *  divideWithOverflowC(uint32_t numerator[], uint32_t denominator[]);
+uint32_t * modulusWithOverflowC(uint32_t numerator[], uint32_t denominator[]);
+
+void montgomery(uint32_t x[], uint32_t y[],  uint32_t z[]);
