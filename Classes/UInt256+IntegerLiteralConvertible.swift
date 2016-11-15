81583f6e110a0cdf416ed9fa39d5e688258b500b
Igor Ranieri Elland <igor@elland.me>
2016-11-15 11:13:56 +0100

    Testing something for Carthage support.

--- Classes/UInt256+IntegerLiteralConvertible.swift
+++ Classes/UInt256+IntegerLiteralConvertible.swift
@@ -8,22 +8,22 @@
 extension UInt256: ExpressibleByIntegerLiteral {
 
     public init(integerLiteral value: IntegerLiteralType) {
-        if CGFLOAT_IS_DOUBLE == 1 {
-            assert(value >= 0, "Unsigned integer should be 0 or larger")
-            assert(value <= 9223372036854775807, "Value too large. Use a decimal string isntead.")
-
-            if value <= 4294967295 {
-                self.init(UInt32(value))
-            } else if value <= 9223372036854775807 {
-                self.init(UInt64(value))
-            } else {
-                fatalError("Something went wrong while creating a UInt256 from \(value).")
-            }
-        } else {
+//        if CGFLOAT_IS_DOUBLE == 1 {
+//            assert(value >= 0, "Unsigned integer should be 0 or larger")
+//            assert(value <= 9223372036854775807, "Value too large. Use a decimal string isntead.")
+//
+//            if value <= 4294967295 {
+//                self.init(UInt32(value))
+//            } else if value <= 9223372036854775807 {
+//                self.init(UInt64(value))
+//            } else {
+//                fatalError("Something went wrong while creating a UInt256 from \(value).")
+//            }
+//        } else {
             assert(value >= 0, "Unsigned integer should be 0 or larger")
             assert(UInt32(value) <= UInt32.max, "Value too large. Use a decimal string isntead.")
 
             self.init(UInt32(value))
-        }
+//        }
     }
 }
